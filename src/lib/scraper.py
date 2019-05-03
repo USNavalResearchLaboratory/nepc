d1090d3795bfc27e2af64ddef8cb026431cfe43f
Paul Adamson <paul.adamson@nrl.navy.mil>
2019-05-03 15:42:39 -0400

    more work on extract_zipf; account for multiple lhs and rhs species

--- src/lib/scraper.py
+++ src/lib/scraper.py
@@ -75,13 +75,13 @@ def writeDataToFile(dataArray,filename):
 	f.close()
 
 def writeMetaDataToFile(filename,specie,process,units_e,units_sigma,ref,
-                        lhs='\\N',rhs='\\N',wavelength='\\N',lhs_v='\\N',rhs_v='\\N',lhs_j='\\N',rhs_j='\\N',background='\\N',lpu='\\N',upu='\\N'):
+                        lhsA='\\N',lhsB='\\N',rhsA='\\N',rhsB='\\N',wavelength='\\N',lhs_v='\\N',rhs_v='\\N',lhs_j='\\N',rhs_j='\\N',background='\\N',lpu='\\N',upu='\\N'):
     f = open(filename, "x")
-    f.write("\t".join(('',specie,process,str(units_e),str(units_sigma),ref,lhs,rhs,str(wavelength),str(lhs_v),str(rhs_v),str(lhs_j),str(rhs_j),background,str(lpu),str(upu))))
+    f.write("\t".join(('',specie,process,str(units_e),str(units_sigma),ref,lhsA,lhsB,rhsA,rhsB,str(wavelength),str(lhs_v),str(rhs_v),str(lhs_j),str(rhs_j),background,str(lpu),str(upu))))
     f.close()
 
 def writeCSToFile(filename,dataArray,specie,process,units_e,units_sigma,ref,
-                  lhs='\\N',rhs='\\N',wavelength='\\N',lhs_v='\\N',rhs_v='\\N',lhs_j='\\N',rhs_j='\\N',background='\\N',lpu='\\N',upu='\\N'):
+                  lhsA='\\N',lhsB='\\N',rhsA='\\N',rhsB='\\N',wavelength='\\N',lhs_v='\\N',rhs_v='\\N',lhs_j='\\N',rhs_j='\\N',background='\\N',lpu='\\N',upu='\\N'):
     writeDataToFile(dataArray,filename)
-    writeMetaDataToFile(filename+".metadata",specie,process,units_e,units_sigma,ref,lhs,rhs,wavelength,lhs_v,rhs_v,lhs_j,rhs_j,background,lpu,upu)
+    writeMetaDataToFile(filename+".metadata",specie,process,units_e,units_sigma,ref,lhsA,lhsB,rhsA,rhsB,wavelength,lhs_v,rhs_v,lhs_j,rhs_j,background,lpu,upu)
 
