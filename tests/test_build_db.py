8d4ffb98ecff23123ceb99f2f5f289b79a6c8ead
Paul Adamson <paul.adamson@nrl.navy.mil>
2019-07-03 18:35:24 -0400

    first draft of testing metadata

--- tests/test_build_db.py
+++ tests/test_build_db.py
@@ -5,6 +5,9 @@ import pandas as pd
 import os
 import pytest
 import platform
+# TODO: fix the database encoding and remove ftfy from here and environment
+from ftfy import fix_encoding
+import csv
 
 # TODO: test that all values in [nepc]/data are in the nepc database
 # TODO: make a test database for testing purposes and check actual values
@@ -40,6 +43,8 @@ def test_csdata_lines(local, dbug):
     cnx.close()
 
 
+# TODO: use @pytest.mark.parametrize decorator to turn this into N tests
+#       instead of N asserts in one test
 def test_data_entered(local, dbug):
     cnx, cursor = nepc.connect(local, dbug)
     if local is False or platform.node() == 'ppdadamsonlinux':
@@ -59,3 +64,53 @@ def test_data_entered(local, dbug):
         assert sigma == pytest.approx(df['sigma'].tolist())
     cursor.close()
     cnx.close()
+
+
+# TODO: use @pytest.mark.parametrize decorator to turn this into N tests
+#       instead of N asserts in one test
+def test_meta_entered(local, dbug):
+    cnx, cursor = nepc.connect(local, dbug)
+    if local is False or platform.node() == 'ppdadamsonlinux':
+        cs_dat_files = pd.read_csv(NEPC_HOME + '/mysql/cs_datfile_prod.tsv',
+                                   delimiter='\t')
+    else:
+        cs_dat_files = pd.read_csv(NEPC_HOME + '/mysql/cs_datfile_local.tsv',
+                                   delimiter='\t')
+
+    for index, row in cs_dat_files.iterrows():
+        cs_id = row['cs_id']
+        met_file = row['filename']
+        if dbug:
+            print(cs_id, met_file)
+        e, sigma = nepc.cs_e_sigma(cursor, cs_id)
+
+        meta_cols = ['specie', 'process', 'units_e',
+                     'units_sigma', 'ref', 'lhsA',
+                     'lhsB', 'rhsA', 'rhsB', 'wavelength',
+                     'lhs_v', 'rhs_v', 'lhs_j', 'rhs_j',
+                     'background', 'lpu', 'upu']
+
+        with open(NEPC_HOME + met_file + ".met", 'r', newline='') as f:
+                reader = csv.reader(f, delimiter='\t')
+                next(reader)
+                meta_disk = list(reader)[0]
+        meta_disk = [meta_disk[i] for i in list(range(1, 18))]
+        for i in [2, 3, 9, 15, 16]:
+            meta_disk[i] = (float(meta_disk[i]) if meta_disk[i] != '\\N'
+                            else meta_disk[i])
+
+        for i in [10, 11, 12, 13]:
+            meta_disk[i] = (int(meta_disk[i]) if meta_disk[i] != '\\N'
+                            else meta_disk[i])
+
+        meta_db = [nepc.cs_metadata(cursor, cs_id)[i]
+                   for i in list(range(1, 18))]
+        meta_db[14] = fix_encoding(meta_db[14])
+        for i in range(len(meta_cols)):
+            if (type(meta_db[i]) is float):
+                assert (pytest.approx(meta_disk[i]) ==
+                        pytest.approx(meta_db[i]))
+            elif meta_db[i] is None:
+                assert meta_disk[i] == '\\N'
+            else:
+                assert meta_disk[i] == meta_db[i]
