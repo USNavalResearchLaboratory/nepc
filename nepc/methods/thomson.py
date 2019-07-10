576af5d931a37c372b4ab03ff643f20d4e38c529
Paul Adamson <paul.adamson@nrl.navy.mil>
2019-07-10 15:12:20 -0400

    fixed linter warnings in thomson.py and adjusted filename of test

--- nepc/methods/thomson.py
+++ nepc/methods/thomson.py
@@ -1,11 +1,13 @@
+# FIXME add reference to where the data is from
 """Module for computing the ionization cross sections from Thomson scattering
 
 Ref/Methods
 -----------
 From Kosarim (2005):
     - Uses Equation 5 to calculate the universal function from electron energy
-    and ionization potential #FIXME add reference to where the data is from
-        - The equation is missing the coulomb force constant (k_e), which must be squared
+    and ionization potential
+        - The equation is missing the coulomb force constant (k_e),
+        which must be squared
     - Uses Equation 11 to calculate the ionization cross section
         - also missing the coulomb force constant (k_e) squared
 From Vasan (1982):
@@ -25,11 +27,13 @@ import numpy as np
 from scipy.constants import elementary_charge as elementary_charge
 
 # Coulomb force constant
-k_e = 8987551787 # m/F
+k_e = 8987551787  # m/F
+
 
 def reduced_energy(energy, ionization_potential):
     """returns a list of reduced energies per Kosarim eq. 5, fig. 1
-    with a list of electron energies (E) and the ionization potential (J_v'_v'')
+    with a list of electron energies (E) and the ionization potential
+    (J_v'_v'')
 
     Parameters
     ----------
@@ -37,13 +41,16 @@ def reduced_energy(energy, ionization_potential):
         Electron energies taken from #FIXME: add reference
 
     ionization_potential: float
-        The ionization potential is taken from the NIST WebBook, Trickl (1989) evaluated
+        The ionization potential is taken from the NIST WebBook, Trickl (1989)
+        evaluated
     """
     return [list_item / ionization_potential for list_item in energy]
 
-def func_ej(cross_section, ionization_potential, n_valence): # using joules instead of eV
-    """sets f(E/J) equal to equation 5, Kosarim (2005), with the added coulomb constant
-    and is used to find the universal function
+
+# TODO: document units correctly (using joules instead of eV)
+def func_ej(cross_section, ionization_potential, n_valence):
+    """sets f(E/J) equal to equation 5, Kosarim (2005), with the added coulomb
+    constant and is used to find the universal function
 
     Parameters
     ----------
@@ -51,13 +58,17 @@ def func_ej(cross_section, ionization_potential, n_valence): # using joules inst
         Ionization cross sections for N2+
 
     ionization_potential: float
-        The ionization potential is taken from the NIST WebBook, Trickl (1989) evaluated
+        The ionization potential is taken from the NIST WebBook, Trickl (1989)
+        evaluated
 
     n_valence: integer
         The number of valence electrons in N2+
     """
-    return [list_item * 10**-20 * (ionization_potential * elementary_charge)**2 /
-            (n_valence * np.pi * elementary_charge**4 * k_e**2) for list_item in cross_section]
+    return [list_item * 10**-20 *
+            (ionization_potential * elementary_charge)**2 /
+            (n_valence * np.pi * elementary_charge**4 * k_e**2)
+            for list_item in cross_section]
+
 
 def func_guess_1(pos_x, a_coef, b_coef, c_coef):
     """this is the first guess for what could fit the f(E/J) data:
@@ -79,6 +90,7 @@ def func_guess_1(pos_x, a_coef, b_coef, c_coef):
     """
     return [(a_coef * (list_item + b_coef)) /
             (np.pi * list_item * (list_item + c_coef)) for list_item in pos_x]
+
 
 def func_guess_2(pos_x, a_coef, b_coef, c_coef, d_coef):
     """this is the second guess for what could fit the f(E/J) data:
@@ -102,4 +114,5 @@ def func_guess_2(pos_x, a_coef, b_coef, c_coef, d_coef):
         Fourth coefficient fitting constant
     """
     return [(a_coef * (list_item + b_coef)) /
-            (np.pi * (list_item + c_coef) * (list_item + d_coef)) for list_item in pos_x]
+            (np.pi * (list_item + c_coef) * (list_item + d_coef))
+            for list_item in pos_x]
--- tests/test_methods_thomson.py
+++ tests/test_methods_thomson.py
@@ -1,24 +1,29 @@
 """Tests the thomson.py module"""
-
 import thomson
 
-#TODO: create a constants file with all the constants we're going to use
-ionization_energy = 15.885 # eV
+# TODO: create a constants file with all the constants we're going to use
+# TODO: test with a set of real values
+# TODO: test that functions raise exceptions when appropriate
+IONIZATION_ENERGY = 15.885  # eV
+
 
 def test_reduced_energy():
     """Checks the object type returned for the reduced energy is a list"""
-    value_type = type(thomson.reduced_energy([1, 2, 3], ionization_energy))
+    value_type = type(thomson.reduced_energy([1, 2, 3], IONIZATION_ENERGY))
     assert value_type == list
+
 
 def test_func_ej():
     """Checks the object typed returned for the universal fit is a list"""
-    value_type = type(thomson.func_ej([1, 2, 3], ionization_energy, 2))
+    value_type = type(thomson.func_ej([1, 2, 3], IONIZATION_ENERGY, 2))
     assert value_type == list
+
 
 def test_guess_1():
     """Checks the object type returned from the guess is a list"""
     value_type = type(thomson.func_guess_1([1, 2, 3], 1, 1, 1))
     assert value_type == list
+
 
 def test_guess_2():
     """Checks the object type returned from the guess is a list"""
