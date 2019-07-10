07acf220d11fc3e06c07b7c2178d6d3a10971df0
dwatkins <darryl.watkins.ctr@nrl.navy.mil>
2019-07-10 14:19:26 -0400

    moved test file to the test dir

--- tests/test_thomson.py
+++ tests/test_thomson.py
@@ -1,13 +1,26 @@
-from nepc.methods import thomson
+"""Tests the thomson.py module"""
 
-def test_length_lists():
-	value = len(thomson.ionen_cross_N2p)
-	assert value == len(thomson.reduced_ion_N2p)
+import thomson
 
-def test_length_lists_2():
-	value = len(thomson.ionen_cross_N2p)
-	assert value == len(thomson.f_of_reduced_ion_N2p)
+#TODO: create a constants file with all the constants we're going to use
+ionization_energy = 15.885 # eV
 
-def test_funcGuess_type():
-	valuetype = type(thomson.coeff_funcGuess_2)
-	assert valuetype == numpy.ndarray
+def test_reduced_energy():
+    """Checks the object type returned for the reduced energy is a list"""
+    value_type = type(thomson.reduced_energy([1, 2, 3], ionization_energy))
+    assert value_type == list
+
+def test_func_ej():
+    """Checks the object typed returned for the universal fit is a list"""
+    value_type = type(thomson.func_ej([1, 2, 3], ionization_energy, 2))
+    assert value_type == list
+
+def test_guess_1():
+    """Checks the object type returned from the guess is a list"""
+    value_type = type(thomson.func_guess_1([1, 2, 3], 1, 1, 1))
+    assert value_type == list
+
+def test_guess_2():
+    """Checks the object type returned from the guess is a list"""
+    value_type = type(thomson.func_guess_2([1, 2, 3], 1, 1, 1, 1))
+    assert value_type == list
