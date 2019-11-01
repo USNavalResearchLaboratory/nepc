3896c4a3a7ee95f0bc39ce69df9327fb95876485
darryl.watkins.ctr@nrl.navy.mil <darryl.watkins.ctr@nrl.navy.mil>
2019-11-01 16:48:32 -0500

    updated pcs to reflect mp

--- nepc/methods/pcs/pcs.py
+++ nepc/methods/pcs/pcs.py
@@ -7,8 +7,11 @@ from numpy import pi as PI
 from nepc import nepc
 from nepc.util.constants import WAVENUMBER_PER_EV as WAVENUMBER_PER_EV
 from nepc.util.constants import K_E as K_E
-from nepc.util.constants import vib_constants_N2
-from nepc.util.constants import vib_constants_N2p
+from nepc.util.constants import N2_DIATOMIC_CONSTANTS as N2_DIATOMIC_CONSTANTS
+from nepc.methods.mp import Te as Te
+from nepc.methods.mp import Tv as Tv
+
+N2_VALENCE = {'N2+(X2Sigmag+)': 2, 'N2+(A2Piu)': 4, 'N2+(B2Sigmau+)': 2}
 
 incident_ee = np.asarray([16.0,16.5,17.0,17.5,18.0,18.5,19.0,19.5,20.0,20.5,21.0,21.5,
                           22.0,22.5,23.0,23.5,24.0,24.5,25.0,30.0,35.0,40.0,45.0,50.0,
@@ -21,47 +24,31 @@ def universal_function(reduced_energy):
     """based off of Paul's fitting coefficients"""
     return np.float64(47.3 * (reduced_energy - 1)/((reduced_energy + 2.4) * (reduced_energy + 9.2)))
 
-def gv_poly(v_level, we_spec, wexe_spec, weye_spec, weze_spec=0, weae_spec=0):
-    """Vibrational energy level polynomial: Table 1 in Laher and Gilmore (1991)"""
-    return (we_spec*(v_level + 1/2) - wexe_spec*(v_level + 1/2)**2 + weye_spec*(v_level + 1/2)**3 +
-            weze_spec*(v_level + 1/2)**4 + weae_spec*(v_level + 1/2)**5)
-
-def te_energy(v_level, t0_spec, we_spec, wexe_spec, weye_spec, weze_spec=0, weae_spec=0):
-    """Energy at minimum of well; Equilibrium term value: Table 1 in Laher and Gilmore (1991)"""
-    return (t0_spec - we_spec/2 + wexe_spec/4 - weye_spec/8 - weze_spec/16 - weae_spec/32)
-
-def tv_energy(v_level, t0_spec, we_spec, wexe_spec, weye_spec, weze_spec=0, weae_spec=0):
-    """Energy difference from minimum to vibrational energy level: Table 1 in Laher and Gilmore (1991)"""
-    return (te_energy(v_level, t0_spec, we_spec, wexe_spec, weye_spec, weze_spec, weae_spec) +
-            gv_poly(v_level, we_spec, wexe_spec, weye_spec, weze_spec, weae_spec))
-
-def pcs(p_state, vp_level, pp_state, vpp_level, fcf, electron_energy='NaN'):
+def pcs(p_state, vp, pp_state, vpp, fcf, electron_energy='NaN'):
     """returns an array of the partial cross section (m^2) of the respective incident electron energy (eV)"""
     if electron_energy=='NaN':
         electron_energy = incident_ee
 
-    if p_state == 'N2(X1Sigmag+)':
-        (p_t0, p_we, p_wexe, p_weye, p_weze, p_weae) = vib_constants_N2[0]
-    if pp_state == 'N2+(X2Sigmag+)':
-        num_valence = 2
-        (pp_t0, pp_we, pp_wexe, pp_weye, pp_weze, pp_weae) = vib_constants_N2p[0]
-    elif pp_state == 'N2+(A2Piu)':
-        num_valence = 4
-        (pp_t0, pp_we, pp_wexe, pp_weye, pp_weze, pp_weae) = vib_constants_N2p[1]
-    elif pp_state == 'N2+(B2Sigmau+)':
-        num_valence = 2
-        (pp_t0, pp_we, pp_wexe, pp_weye, pp_weze, pp_weae) = vib_constants_N2p[2]
+    valence = N2_VALENCE[pp_state]
 
-    tv_vp = tv_energy(vp_level, p_t0, p_we, p_wexe, p_weye, p_weze, p_weae)
-    tv_vpp = tv_energy(vpp_level, pp_t0, pp_we, pp_wexe, pp_weye, pp_weze, pp_weae)
+    p_To = N2_DIATOMIC_CONSTANTS[p_state]['To']
+    p_we = N2_DIATOMIC_CONSTANTS[p_state]['we']
+    p_wexe = N2_DIATOMIC_CONSTANTS[p_state]['wexe']
 
-    tv_vppvp = t_vpp - t_vp
-    tv_vppvp_ev = tvpp_vp / WAVENUMBER_PER_EV
+    pp_To = N2_DIATOMIC_CONSTANTS[pp_state]['To']
+    pp_we = N2_DIATOMIC_CONSTANTS[pp_state]['we']
+    pp_wexe = N2_DIATOMIC_CONSTANTS[pp_state]['wexe']
+
+    Tv_vp = Tv(vp, p_To, p_we, p_wexe)
+    Tv_vpp = Tv(vpp, pp_To, pp_we, pp_wexe)
+
+    Tv_vppvp = Tv_vpp - Tv_vp
+    Tv_vppvp_ev = Tv_vppvp / WAVENUMBER_PER_EV
 
     pcs_list = []
     for i in electron_energy:
-        sigma_pp_vpp = num_valence * K_E**2 * PI * ELEMENTARY_CHARGE**4 * universal_function(i/tv_vppvp_ev) * fcf / (tv_vppvp_ev * ELEMENTARY_CHARGE)**2
-        pcs_list.append([i, sigma_pp_vpp])
+        sigma = np.float64(valence * K_E**2 * PI * ELEMENTARY_CHARGE**4 * universal_function(i/tv_vppvp_ev) * fcf / (tv_vppvp_ev * ELEMENTARY_CHARGE)**2)
+        pcs_list.append([i, sigma])
 
     return pcs_list
 
