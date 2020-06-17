264951ce4c7fb086bbbd1cadcb0b5da8b36c80cb
Paul Adamson <paul.adamson@nrl.navy.mil>
2020-06-17 17:59:22 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,132 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
 import os
 import sys
+from shutil import rmtree
 
-_here = os.path.abspath(os.path.dirname(__file__))
+from setuptools import find_packages, setup, Command
 
-if sys.version_info[0] < 3:
-    with open(os.path.join(_here, 'README.md')) as f:
-        long_description = f.read()
+# Package meta-data.
+NAME = 'nepc'
+DESCRIPTION = 'Build, access, and explore a NEPC database.'
+URL = ''
+EMAIL = 'paul.adamson@nrl.navy.mil'
+AUTHOR = 'Paul Adamson'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'numpy', 'scipy', 'ipython', 'ipython_genutils', 'jpeg', 'jupyter',
+    'jupyter_client', 'pandas', 'matplotlib', 'tabulate', 'quadpy', 'numpydoc',
+    'quadpy', 'pdfminer.six', 'pdfplumber', 'mysql-connector-python'
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
 else:
-    with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
+    about['__version__'] = VERSION
 
-version = {}
-with open(os.path.join(_here, 'nepc', 'version.py')) as f:
-    exec(f.read(), version)
 
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+        
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name='nepc',
-    version=version['__version__'],
-    description='Build, access, and explore a NEPC database.',
-    long_description=('Build, access, and explore an NRL Evaluated Plasma ' +
-                      'Chemistry (NEPC) database.'),
-    author='Paul Adamson',
-    author_email='paul.adamson@nrl.navy.mil',
-    license='',
-    packages=['nepc', 'nepc.util'],
-    #   no dependencies in this example
-    #   install_requires=[
-    #       'dependency==1.2.3',
-    #   ],
-    #   no scripts in this example
-    #   scripts=['bin/a-script'],
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    # packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    py_modules=['turbopy'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
     include_package_data=True,
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest", "pytest-cov"],
     classifiers=[
-        'Development Status :: 1 - Development',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.7'],
-    )
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
