d19e73fd0543ca5a4716cb075a655781f70a2a6f
Paul Adamson <paul.adamson@nrl.navy.mil>
2020-10-01 07:33:24 -0400

    implement initialize step of curate template

--- nepc/curate/curate.py
+++ nepc/curate/curate.py
@@ -8,29 +8,44 @@ Provides templates for curating raw and external (e.g. LxCAT) cross section data
 - write data to nepc formatted input files (i.e. .dat, .met, .mod)
 """
 from abc import ABC, abstractclassmethod, abstractmethod
-
+from typing import List, Tuple
+from nepc.util import util
+from nepc.util import parser
 
 class CurateCS(ABC):
     """Template method that contains the skeleton for curating cross section data.
     """
 
 
-    def curate(self) -> None:
+
+    def curate(self, datadir: str, outdir: str) -> Tuple[List[str], str, str]:
         """Skeleton of curation process.
         """
-
-        self.initialize()
+        datafiles, next_cs_id, next_csdata_id = self.initialize(datadir, outdir)
         self.get_csdata()
         self.clean_csdata()
         self.augment_csdata()
         self.verify_csdata()
         self.write_csdata()
         self.finalize()
+        return datafiles, next_cs_id, next_csdata_id
+
+    def initialize(self, datadir: str, outdir: str) -> Tuple[List[str], str, str]:
+        datafiles = [datadir + '/Phelps.txt']
+
+        util.rmdir(outdir)
+        util.mkdir(outdir)
+
+        debug = True
+
+        if debug:
+            next_cs_id, next_csdata_id = (424, 202224)
+        else:
+            next_cs_id, next_csdata_id = parser.get_next_ids()
+
+        return datafiles, next_cs_id, next_csdata_id
 
 
-    @abstractmethod
-    def initialize(self) -> None:
-        pass
 
 
     @abstractmethod
@@ -69,10 +84,6 @@ class CurateLxCAT(CurateCS):
         [description]
     """
 
-    def initialize(self) -> None:
-        pass
-
-
     def get_csdata(self) -> None:
         pass
 
@@ -97,9 +108,11 @@ class CurateLxCAT(CurateCS):
     def __str__(self) -> str:
         return "LxCAT curation process"
 
-def curate_client(curate_cs: CurateCS) -> None:
+def curate_client(curate_cs: CurateCS, datadir: str, outdir: str) -> Tuple[List[str], str, str]:
     """Client code that calls the CurateCS template to execute the curation process.
     """
 
     print(f"Executing {curate_cs}.")
-    curate_cs.curate()
+    datafiles, next_cs_id, next_csdata_id = curate_cs.curate(datadir, outdir)
+    return datafiles, next_cs_id, next_csdata_id
+
