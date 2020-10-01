308f2e246fa53a7a09abde7714067ae1f7eacd90
Paul Adamson <paul.adamson@nrl.navy.mil>
2020-10-01 09:13:01 -0400

    finish first version of curate module

--- nepc/curate/curate.py
+++ nepc/curate/curate.py
@@ -11,45 +11,44 @@ from abc import ABC, abstractclassmethod, abstractmethod
 from typing import List, Tuple
 from nepc.util import util
 from nepc.util import parser
+import numpy as np
+import re
 
 class CurateCS(ABC):
     """Template method that contains the skeleton for curating cross section data.
     """
 
 
-
-    def curate(self, datadir: str, outdir: str) -> Tuple[List[str], str, str]:
+    def curate(self, datadir: str, outdir: str, states: dict, title: str, debug=False, **next_ids) -> None:
         """Skeleton of curation process.
         """
-        datafiles, next_cs_id, next_csdata_id = self.initialize(datadir, outdir)
-        self.get_csdata()
-        self.clean_csdata()
-        self.augment_csdata()
+        datafiles, next_cs_id, next_csdata_id = self.initialize(datadir, outdir, debug, **next_ids)
+        csdata = self.get_csdata(datafiles, debug=debug)
+        csdata = self.clean_csdata(csdata, debug=debug)
+        csdata = self.augment_csdata(csdata, outdir, states, title)
         self.verify_csdata()
-        self.write_csdata()
-        self.finalize()
-        return datafiles, next_cs_id, next_csdata_id
+        next_cs_id, next_csdata_id = self.write_csdata(csdata, next_cs_id, next_csdata_id)
+        self.finalize(next_cs_id, next_csdata_id)
 
-    def initialize(self, datadir: str, outdir: str) -> Tuple[List[str], str, str]:
+
+    def initialize(self, datadir: str, outdir: str, debug=False, **next_ids) -> Tuple[List[str], str, str]:
         datafiles = [datadir + '/Phelps.txt']
 
         util.rmdir(outdir)
         util.mkdir(outdir)
 
-        debug = True
-
         if debug:
-            next_cs_id, next_csdata_id = (424, 202224)
+            next_cs_id, next_csdata_id = (next_ids["next_cs_id"], next_ids["next_csdata_id"])
         else:
             next_cs_id, next_csdata_id = parser.get_next_ids()
+
+        print(f"next_cs_id: {next_cs_id}\nnext_csdata_id: {next_csdata_id}")
 
         return datafiles, next_cs_id, next_csdata_id
 
 
-
-
     @abstractmethod
-    def get_csdata(self) -> None:
+    def get_csdata(self):
         pass
 
     @abstractmethod
@@ -84,35 +83,166 @@ class CurateLxCAT(CurateCS):
         [description]
     """
 
-    def get_csdata(self) -> None:
-        pass
+    def value(self, csdata_i, key):
+        float_keys = ['threshold']
+        int_keys = ['lhs_v', 'rhs_v']
 
-    def clean_csdata(self) -> None:
-        pass
+        if key in csdata_i.keys():
+            if csdata_i[key] == '':
+                if key in float_keys or key in int_keys:
+                    return '-1'
+                else:
+                    return '\\N'
+            else:
+                return str(csdata_i[key])
+        else:
+            if key in float_keys or key in int_keys:
+                return '-1'
+            else:
+                return '\\N'
+
+    def print_csdata_table(self, keys, csdata):
+        print('\t'.join(keys))
+        print('===============================================================================')
+        for cs, i in zip(csdata, range(len(csdata))):
+            print('\t'.join([self.value(cs, key) for key in keys]))
+
+    def get_csdata(self, datafiles, debug=False):
+        csdata = parser.parse(datafiles[0], debug=debug)
+        print(f"Length of csdata: {len(csdata)}")
+        return csdata
+
+    def clean_csdata(self, csdata, debug=False):
+        def remove_zeros_at_end(csdata):
+            for cs in csdata:
+                i = len(cs['data']) - 1
+                while cs['data'][i][1] == 0.0:
+                    print('removing {} from csdata[{}][\'data\']'.format(cs['data'].pop(i), i))
+                    i -= 1
+
+        def remove_zeros_at_beginning(csdata):
+            for cs in csdata:
+                while cs['data'][0][1] == 0.0 and cs['data'][1][1] == 0.0:
+                    print('removing {} from csdata[{}][\'data\']'.format(cs['data'].pop(0), 0))
+        remove_zeros_at_beginning(csdata)
+        remove_zeros_at_end(csdata)
+        keys = ['specie',
+                'process',
+                'units_e',
+                'units_sigma',
+                'lhs_a',
+                'rhs_a',
+                'lhs_v',
+                'rhs_v',
+                'threshold',
+                'nepc_filename']
+        
+        self.print_csdata_table(keys, csdata)
+        self.print_csdata_table(['background'], csdata)
+        return csdata
 
 
-    def augment_csdata(self) -> None:
-        pass
-
+    def augment_csdata(self, csdata, outdir, states, title):
+        for cs, i in zip(csdata, range(len(csdata))):
+            cs['specie'] = self.value(cs, 'target')
+            cs['units_e'] = '1.0'
+            cs['units_sigma'] = '1.0'
+            cs['background'] = self.value(cs, 'comment')
+            cs['nepc_filename'] = outdir + '/' + title + '_' + str(i)
+            cs['data'] = np.asarray(cs['data'])
+            cs['ref'] = self.value(cs, 'ref')
+            cs['threshold'] = self.value(cs, 'threshold')
+            cs['lhs_v'] = self.value(cs, 'lhs_v')
+            cs['rhs_v'] = self.value(cs, 'rhs_v')
+    
+            if (cs['kind'] == 'IONIZATION' and cs['product'] == 'N2^+'):
+                cs['process'] = 'ionization_total'
+                cs['lhs_a'] = states['N2(X1)']
+                cs['rhs_a'] = 'N2+'
+                cs['models'] = ['phelps', 'phelps_min', 'phelps_min2', 'phelps_min2_dr']
+            elif (cs['kind'] == 'IONIZATION' and cs['product'] == 'N2^+(B2SIGMA)'):
+                cs['process'] = 'ionization'
+                cs['lhs_a'] = states['N2(X1)']
+                cs['rhs_a'] = states['N2+(B2)']
+                cs['models'] = ['phelps']
+            elif (cs['kind'] == 'EXCITATION' and
+                cs['product'] == 'N2(rot)' and
+                'SLAR' in cs['background']):
+                cs['process'] = 'excitation'
+                cs['lhs_a'] = states['N2(X1)']
+                cs['rhs_a'] = 'N2(X1Sigmag+)_jSLAR'
+                cs['models'] = ['phelps', 'phelps_min2', 'phelps_min2_dr']
+            elif (cs['kind'] == 'EXCITATION' and
+                  cs['product'] == 'N2(rot)' and
+                  'SCHULZ' in cs['background']):
+                cs['process'] = 'excitation'
+                cs['lhs_a'] = states['N2(X1)']
+                cs['rhs_a'] = 'N2(X1Sigmag+)_jSCHULZ'
+                cs['models'] = ['phelps']
+            elif (cs['kind'] == 'EFFECTIVE'):
+                cs['process'] = 'total'
+                cs['lhs_a'] = 'N2'
+                cs['rhs_a'] = 'N2'
+                cs['models'] = ['phelps', 'phelps_min', 'phelps_min2', 'phelps_min2_dr']
+            elif (cs['kind'] == 'EXCITATION' and
+                  ('v0-4' in cs['product'] or 'v5-9' in cs['product'] or 'v10-' in cs['product'])):
+                cs['process'] = 'excitation'
+                cs['lhs_a'] = states['N2(X1)']
+                cs['rhs_a'] = states[cs['product']]
+                cs['models'] = ['phelps']
+            elif (cs['kind'] == 'EXCITATION' and
+                  re.search('v[0-9]', cs['product']) is not None):
+                cs['process'] = 'excitation_v'
+                cs['lhs_a'] = states['N2(X1)']
+                cs['rhs_a'] = states['N2(X1)']
+                cs['lhs_v'] = '0'
+                cs['rhs_v'] = cs['product'][4]
+                cs['models'] = ['phelps']
+            elif (cs['kind'] == 'EXCITATION'):
+                cs['process'] = 'excitation'
+                cs['lhs_a'] = states['N2(X1)']
+                cs['rhs_a'] = states[cs['product']]
+                cs['models'] = ['phelps']
+        return csdata
 
     def verify_csdata(self) -> None:
         pass
 
-    def write_csdata(self) -> None:
-        pass
+    def write_csdata(self, csdata, next_cs_id, next_csdata_id):
+        for cs in csdata:   
+            next_csdata_id = parser.write_data_to_file(data_array=cs['data'],
+                                                       filename=cs['nepc_filename']+'.dat',
+                                                       start_csdata_id=next_csdata_id)
+            next_cs_id = parser.write_metadata_to_file(filename=cs['nepc_filename']+'.met',
+                                                       cs_id=next_cs_id,
+                                                       specie=cs['specie'],
+                                                       process=cs['process'],
+                                                       lhs_a=cs['lhs_a'],
+                                                       rhs_a=cs['rhs_a'],
+                                                       lhs_v=cs['lhs_v'],
+                                                       rhs_v=cs['rhs_v'],
+                                                       units_e=cs['units_e'],
+                                                       units_sigma=cs['units_sigma'],
+                                                       threshold=cs['threshold'],
+                                                       ref=cs['ref'],
+                                                       background=cs['background'])
+            parser.write_models_to_file(filename=cs['nepc_filename']+'.mod',
+                                        models_array=cs['models'])
+        return next_cs_id, next_csdata_id
 
-    def finalize(self) -> None:
-        pass
+
+    def finalize(self, next_cs_id, next_csdata_id) -> None:
+        parser.write_next_id_to_file(next_cs_id, next_csdata_id)
+        print(f'next_cs_id: {next_cs_id}\nnext_csdata_id: {next_csdata_id}')
 
 
     def __str__(self) -> str:
         return "LxCAT curation process"
 
-def curate_client(curate_cs: CurateCS, datadir: str, outdir: str) -> Tuple[List[str], str, str]:
+def curate_client(curate_cs: CurateCS, datadir: str, outdir: str, states: dict, title: str, debug=False, **next_ids) -> None:
     """Client code that calls the CurateCS template to execute the curation process.
     """
 
     print(f"Executing {curate_cs}.")
-    datafiles, next_cs_id, next_csdata_id = curate_cs.curate(datadir, outdir)
-    return datafiles, next_cs_id, next_csdata_id
-
+    
+    curate_cs.curate(datadir, outdir, states, title, debug, **next_ids)
