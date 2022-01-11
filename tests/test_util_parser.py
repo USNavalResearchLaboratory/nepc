d19158a47e3693538778d1de25481e76a2c94f97
Paul Adamson <paul.adamson@nrl.navy.mil>
2022-01-11 14:56:34 -0500

    streamline CI

--- tests/test_util_parser.py
+++ tests/test_util_parser.py
@@ -1,6 +1,6 @@
 """Tests for nepc.util.parser.py"""
 import pytest
-from nepc.util import parser
+from nepc.util.parser import format_model
 import nepc
 import hashlib
 
@@ -9,7 +9,7 @@ def test_format_model_exception_for_unsupported_format():
     """Verify format_model raises an exception if format is
     not supported"""
     with pytest.raises(Exception) as e:
-        assert parser.format_model(model=None, type='bsr')
+        assert format_model(model=None, type='bsr')
     assert str(e.value) == 'type bsr is not supported'
 
 
@@ -17,7 +17,7 @@ def test_format_model_exception_for_unsupported_model():
     """Verify format_model raises an exception if model is
     not a nepc.Model"""
     with pytest.raises(Exception) as e:
-        assert parser.format_model(model="str_not_model")
+        assert format_model(model="str_not_model")
     assert str(e.value) == 'model str_not_model is not supported' 
 
 
@@ -25,7 +25,7 @@ def test_format_model_exception_for_unsupported_model():
 def test_format_model_write_file(nepc_connect, tmpdir):
     file = tmpdir.join('lxcat.txt')
     fict = nepc.Model(nepc_connect[1], "fict")
-    parser.format_model(fict, type='lxcat', filename=str(file))
+    format_model(fict, type='lxcat', filename=str(file))
     lines = file.read().splitlines()
     assert len(lines) == 1951
     assert lines[0] == 'EXCITATION'
