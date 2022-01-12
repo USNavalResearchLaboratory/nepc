b8585062ddcc45bc429ab5b1efe3edb845973a26
Grace Tang <49345480+gracetangg@users.noreply.github.com>
2022-01-12 12:02:47 -0500

    Add `__str__` method for CS and Model class (#76)
    * add python interactive version of tests/data/eda/01_lxcat_n2_fict.ipynb
    
    * add str method for CS class
    
    * add pytest for str method
    
    * Delete .gitignore
    
    * Delete Project_Default.xml
    
    * Delete profiles_settings.xml
    
    * Delete misc.xml
    
    * Delete modules.xml
    
    * Delete nepc.iml
    
    * Delete vcs.xml
    
    * use hard wired values for tests
    
    Co-authored-by: Paul Adamson <5460406+padamson@users.noreply.github.com>

--- tests/test_nepc.py
+++ tests/test_nepc.py
@@ -84,8 +84,22 @@ def test_CS_class(nepc_connect):
     assert isinstance(cs.data["sigma"], list)
     assert isinstance(cs.data["sigma"][0], float)
     # test len()
-    # FIXME: use length of data file to get length of sampled cross section
     assert len(cs) == 21
+    # test str()
+    message = "cross section id: 1\n" \
+            + "process: excitation\n" \
+            + "reaction: E + N2(X1Sigmag+) -> E + N2(X1Sigmag+)_jSCHULZ\n" \
+            + "threshold: 0.02 eV\n" \
+            + "ref: N/A\n" \
+            + "background: SCHULZ Lorem ipsum dolor sit amet, " \
+            + "consectetur adipiscing elit, sed do eiusmod tempor " \
+            + "incididunt ut labore et dolore magna aliqua. " \
+            + "Ut enim ad minim veniam, quis nostrud exercitation ullamco " \
+            + "laboris nisi ut aliquip ex ea commodo consequat. " \
+            + "Duis aute irure dolor in reprehenderit in voluptate " \
+            + "velit esse cillum dolore eu fugiat nulla pariatur.\n"
+    assert(str(cs) == message)
+    # FIXME: use length of data file to get length of sampled cross section
 
 
 @pytest.mark.usefixtures("nepc_connect")
